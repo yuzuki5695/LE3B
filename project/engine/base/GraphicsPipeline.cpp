03c34cfd035afd7e1b2c50392506436c93095b94
yuzuki5695 <146906397+yuzuki5695@users.noreply.github.com>
2025-04-18 11:06:03 +0900

    CG3_02_00の移植完了

--- project/engine/base/GraphicsPipeline.cpp
+++ project/engine/base/GraphicsPipeline.cpp
@@ -243,7 +243,7 @@ void GraphicsPipeline::RootSignatureGenerateObject3d() {
     /*----------------------------------------------------------------------------------*/
     /*---------------------------------RootParameter作成---------------------------------*/
     /*----------------------------------------------------------------------------------*/
-    D3D12_ROOT_PARAMETER rootParameters[4] = {};
+    D3D12_ROOT_PARAMETER rootParameters[5] = {};
     rootParameters[0].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;// CBVを使う
     rootParameters[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;// PixelShaderで使う
     rootParameters[0].Descriptor.ShaderRegister = 0;// レジスタ番号0を使う
@@ -260,6 +260,10 @@ void GraphicsPipeline::RootSignatureGenerateObject3d() {
     rootParameters[3].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;// CBVを使う
     rootParameters[3].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;// PixelShaderで使う
     rootParameters[3].Descriptor.ShaderRegister = 1;// レジスタ番号1を使う
+
+    rootParameters[4].ParameterType = D3D12_ROOT_PARAMETER_TYPE_CBV;// CBVを使う
+    rootParameters[4].ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;// PixelShaderで使う
+    rootParameters[4].Descriptor.ShaderRegister = 2;// レジスタ番号2を使う
 
     /*----------------------------------------------------------------------------------*/
     /*---------------------------------RootSignature作成---------------------------------*/
